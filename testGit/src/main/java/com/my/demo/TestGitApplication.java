df984ca2341b1cbe50ddb6306574d9e99eff06f1
anRan <16521666675@qq.com>
2018-03-23 13:13:06 +0800

    add testGit

---
+++ testGit/src/main/java/com/my/demo/TestGitApplication.java
@@ -0,0 +1,12 @@
+package com.my.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestGitApplication.class, args);
+	}
+}
