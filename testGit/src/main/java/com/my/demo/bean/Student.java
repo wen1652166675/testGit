b3f17909b45b302716f960c0c2f398213371c275
anRan <16521666675@qq.com>
2018-03-23 19:01:31 +0800

    fourth

---
+++ testGit/src/main/java/com/my/demo/bean/Student.java
@@ -0,0 +1,47 @@
+package com.my.demo.bean;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+	
+	@Id
+	@GeneratedValue
+	private int id;
+	private String name;
+	private String className;
+	
+	public Student() {
+		
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getClassName() {
+		return className;
+	}
+	public void setClassName(String className) {
+		this.className = className;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", className=" + className + "]";
+	}
+	
+	
+	
+}
