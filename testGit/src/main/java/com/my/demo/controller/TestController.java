4f23ba5f61507476f161220c4d38dd0cf251fabc
anRan <16521666675@qq.com>
2018-03-25 11:16:35 +0800

    add findAll

--- testGit/src/main/java/com/my/demo/controller/TestController.java
+++ testGit/src/main/java/com/my/demo/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.my.demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,5 +29,10 @@ public class TestController {
 		
 		return sr.save(s);
 		
+	}
+	
+	@GetMapping("/findAll")
+	public List<Student> findAll() {
+		return sr.findAll();
 	}
 }
